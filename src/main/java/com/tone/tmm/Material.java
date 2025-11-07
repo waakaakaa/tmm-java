dbf054e4853a36970323c96cfdb11871009b5b24
Xin Zhang <341852201@qq.com>
2025-11-07 10:25:39 +0800

    Add Material enum with various material types

---
+++ src/main/java/com/tone/tmm/Material.java
@@ -0,0 +1,16 @@
+package com.tone.tmm;
+
+public enum Material {
+    TY_Al2O3, 
+    TY_Si, 
+    TY_Si_new, 
+    TY_Si3N4, 
+    TY_Si3N4_new, 
+    TY_SiO2, 
+    TY_SiO2_new, 
+    TY_Ta2O5, 
+    TY_Ta2O5_new, 
+    InP23191, 
+    InP18515, 
+    Air
+}
